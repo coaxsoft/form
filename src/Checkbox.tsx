4f4284a703978366789de3dc106a58795c76f280
Vadym Perevozniuk <perevozniuk.v@gmail.com>
2020-06-08 15:17:07 +0300

    added label type to interface

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -7,6 +7,7 @@ import formElementWrapper, { WrapperFormElementProps } from "./FormElementWrappe
 export interface Props extends WrapperFormElementProps {
   className?: string;
   children?: React.ReactNode;
+  label?: string
 }
 
 const Checkbox = (props: Props & React.HTMLProps<HTMLInputElement>) => {
@@ -20,16 +21,12 @@ const Checkbox = (props: Props & React.HTMLProps<HTMLInputElement>) => {
   if (errors[name]) classNames.push("coax-form--checkbox-el__error");
 
   return (
-    // <div className="coax-form--checkbox-wrapper">
-    //   <input id={name} className={classNames.join(" ")} name={name} ref={register} type="checkbox" />
-    //   <label className="coax-form--checkbox-label" htmlFor={name}>{props.children || null}</label>
-    // </div>
     <div className="coax-form--checkbox-wrapper">
       <label className="coax-form--checkbox-label" htmlFor={name}>
-        {/*{props.children || null}*/}
         {label}
         <input id={name} className={classNames.join(" ")} name={name} ref={register} type="checkbox"/>
         <span className="checkmark"/>
+        {props.children || null}
       </label>
     </div>
 
